75967cf12dca81e61f764e5dc3bf7b21de58bb32
iki1uc <entertrainer@web.de>
2026-08-16 23:13:56 +0200

    Delete 1 directory

--- 1/cu.js
+++
@@ -1,7 +0,0 @@
-function atlantisTor(vector) {
-    switch(vector) {
-        case "TOR": return "ROOT";          // Rückkehr
-        case "TOR_invert": return "BREAK";  // Gegen-Zeit
-        case "TOR_echo": return "ECHO";     // Spiegelung
-    }
-}
