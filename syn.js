273f34256a6c1ed76be1a556fb29b2f2a4ca21f2
iki1uc <entertrainer@web.de>
2026-08-16 23:06:43 +0200

    Update syn.js

--- syn.js
+++ syn.js
@@ -1,8 +1,3 @@
-function respoRouter(SYN, QUANT) {
-    const phi = 1.618;
-    const R = (SYN + QUANT) / phi;
-
-    if (R > 0.66) return ["OK", "FLOW", "ROOT"];
-    if (R < 0.33) return ["BREAK_invert", "NOK", "ECHO"];
-    return ["NOK", "BREAK_invert", "ECHO"];
+export function syn(input) {
+    return input * 3; // deine 3TEL-Regel
 }
