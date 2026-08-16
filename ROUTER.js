ece1b6eb0937dafb296f5cfc5b7043d3be85774f
iki1uc <entertrainer@web.de>
2026-08-16 23:18:49 +0200

    Update ROUTER.js

--- ROUTER.js
+++ ROUTER.js
@@ -1,37 +1,10 @@
-import { dreiecke } from "./dreiecke.js";
-import { kreis } from "./kreis.js";
-import { zeit } from "./zeit.js";
-import { invert } from "./invert.js";
+import { dreigeologramm } from "./3eck.js";
 
-export function router(SYN, QUANT) {
+export function ROUTER(SYN, QUANT) {
   const phi = 1.618;
   const R = (SYN + QUANT) / phi;
 
-  if (R > 0.66) {
-    return {
-      bahn: "TOP",
-      dreiecke: dreiecke.top,
-      respo: ["OK", "FLOW", "ROOT"],
-      vector: kreis.vector,
-      zeit: zeit.forward
-    };
-  }
-
-  if (R < 0.33) {
-    return {
-      bahn: "BOTTOM",
-      dreiecke: dreiecke.bottom,
-      respo: ["NOK", "BREAK", "ECHO"],
-      vector: kreis.vector,
-      zeit: zeit.neutral
-    };
-  }
-
-  return {
-    bahn: "TOR",
-    dreiecke: invert.tor,
-    respo: ["TOR", "TOR_INVERT", "TOR_ECHO"],
-    vector: invert.vector,
-    zeit: zeit.reverse
-  };
+  if (R > 0.66) return dreigeologramm.top;
+  if (R < 0.33) return dreigeologramm.bottom;
+  return dreigeologramm.mid;
 }
