c00eaee877deb72f71418f7d7f1476823e91af1b
iki1uc <entertrainer@web.de>
2026-08-16 23:17:21 +0200

    Update 3eck.js

--- 3eck.js
+++ 3eck.js
@@ -1,4 +1,4 @@
-export const dreiecke = {
+export const dreigeologramm = {
   top: ["△1_OK", "△2_FLOW", "△3_ROOT"],
   mid: ["△4_TOR", "△5_TOR_INVERT", "△6_TOR_ECHO"],
   bottom: ["△7_NOK", "△8_BREAK", "△9_ECHO"]
