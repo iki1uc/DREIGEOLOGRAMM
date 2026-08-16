d1c0fa86f490e84168a8187f111687f776ce84c9
iki1uc <entertrainer@web.de>
2026-08-16 23:10:32 +0200

    Create md.js

---
+++ md.js
@@ -0,0 +1,12 @@
+export const filetypes = {
+  impulse: "IMPULSE",
+  symbol: "SYMBOL",
+  raw: "RAW",
+  mode: "MODE",
+  room: "ROOM",
+  run: "RUN",
+  sys: "SYS",
+  html: "HTML",
+  md: "MARKDOWN",
+  json: "JSON"
+};
